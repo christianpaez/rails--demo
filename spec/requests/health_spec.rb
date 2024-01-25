5cdfb08a8b084bc11e9d05918bbee3c9e7ad13ef
Christian <parrot@localhost.localdomain>
2024-01-25 10:14:46 -0500

    rspec and swagger

---
+++ spec/requests/health_spec.rb
@@ -0,0 +1,21 @@
+require 'swagger_helper'
+
+RSpec.describe 'health', type: :request do
+
+  path '/health' do
+
+    get('check health') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
